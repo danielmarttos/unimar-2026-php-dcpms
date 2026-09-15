abe2cdabed42f45c507542a2493031d2d9778b79
DDANIEL! <dmarttos8@gmail.com>
2026-09-14 23:07:03 -0300

    atividade1

--- PHP/atividadesP1/questao1/index.php
+++ PHP/atividadesP1/questao1/index.php
@@ -1,9 +1,38 @@
 <?php
 
+//Construção de classes e funções
+
 class Funcionario {
-private string $nome;
-private string $cargo;
-private float $salario;
-// construtor
-// getters: getNome(), getCargo(), getSalario()
+    private string $nome;
+    private string $cargo;
+    private float $salario;
+
+    public function __construct(string $nome, string $cargo, float $salario) {
+
+        $this->nome = $nome;
+        $this->cargo = $cargo;
+        $this->salario = $salario;
+
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getCargo(): string {
+        return $this->cargo;
+    }
+
+    public function getSalario(): float {
+        return $this->salario;
+    }
+
 }
+
+//Testes comprovando funcionamento!
+
+$funcionario = new Funcionario("Roberto","Chefe",7000.99);
+
+echo $funcionario->getNome();
+echo $funcionario->getCargo();
+echo $funcionario->getSalario();
